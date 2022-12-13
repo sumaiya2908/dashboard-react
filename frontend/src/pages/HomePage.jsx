8dd072e57faede693b00649ec5e44f270bec18d5
unknown <sumaiyahas2908@gmail.com>
2022-12-13 10:16:01 +0530

    feat: home page with dropdown and chart

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Chart from '../components/Chart'
+
+const HomePage = () => {
+	const [data, setData] = useState([])
+
+	useEffect(() => {
+		axios({
+			url: "http://localhost:3500/get-data",
+			method: "get",
+		}).then(({ data }) => {
+			setData(data);
+		});
+	}, [])
+
+	return (
+		<div className='homepage'>
+			<h3>Device Dashboard</h3>
+			<Chart data={data} />
+		</div>
+	)
+}
+
+export default HomePage
