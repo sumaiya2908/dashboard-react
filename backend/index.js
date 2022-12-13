f6b3d0d4223c3176c98d8dd98809c1e8bd2c24ab
unknown <sumaiyahas2908@gmail.com>
2022-12-13 10:10:35 +0530

    feat: connect to mongodb

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,9 @@ const app = express()
 
 app.use(cors())
 
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI)
 
 app.listen(3500, () => {
 	console.log("Server started at 3500")
