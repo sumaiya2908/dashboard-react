8e50f9f2027a1d8bd4474c822618b9158a3755bc
unknown <sumaiyahas2908@gmail.com>
2022-12-13 10:11:17 +0530

    feat: add api to get data from db

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,14 @@ require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI)
 
+const db = mongoose.connection
+
+app.get("/get-data", (req, res) => {
+	db.collection('device-data').find({}).toArray(function (err, data) {
+		res.send(data)
+	})
+});
+
 app.listen(3500, () => {
 	console.log("Server started at 3500")
 })
